5b3bcfee90693cac0b41c61f63a1661d82a36911
Nail Girfanov <nail.ildarovich@gmail.com>
2025-02-16 19:08:11 +0500

    changes to the project

--- gendiff/formatters/__init__.py
+++ gendiff/formatters/__init__.py
@@ -1,4 +1,15 @@
-from gendiff.formatters.select_formatter import select_formatter as main
+from gendiff.formatters.json import json_formatter
+from gendiff.formatters.plain import plain
+from gendiff.formatters.stylish import stylish
 
-if __name__ == '__main__':
-    main()
+
+def select_formatter(diff, format_name):
+    match format_name:
+        case 'stylish':
+            return stylish(diff)
+        case 'plain':
+            return plain(diff)
+        case 'json':
+            return json_formatter(diff)
+        case _:
+            return stylish(diff)
--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,5 +1,5 @@
 from gendiff.build_diff import build_diff
-from gendiff.formatters.__init__ import main
+from gendiff.formatters.__init__ import select_formatter
 from gendiff.parser import parser
 
 
@@ -7,5 +7,5 @@ def generate_diff(path_file1, path_file2, format_name='stylish'):
     file1 = parser(path_file1)
     file2 = parser(path_file2)
     diff = build_diff(file1, file2)
-    select_formatters = main(diff, format_name)
+    select_formatters = select_formatter(diff, format_name)
     return select_formatters
